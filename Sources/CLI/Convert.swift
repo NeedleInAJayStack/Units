4e14254c699fca3232c3c75f5fa327f723268ac4
Jay Herron <NeedleInAJayStack@protonmail.com>
2023-01-31 23:42:55 -0700

    doc: CLI documentation improvements

--- Sources/CLI/Convert.swift
+++ Sources/CLI/Convert.swift
@@ -5,29 +5,32 @@ struct Convert: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "Convert a measurement to a specified unit.",
         discussion: """
-        This command uses the unit and measurement serialization format. For more details, see
+        Run `unit list` to see the supported unit symbols and names. Unless arguments are wrapped \
+        in quotes, the `*` character may need to be escaped.
+
+        The arguments use the unit and measurement serialization format. For more details, see \
         https://github.com/NeedleInAJayStack/Units/blob/main/README.md#serialization
 
-        Note that for convenience, you may use an underscore `_` to represent the normally 
-        serialized space. Also, unless arguments are wrapped in quotes, the `*` character may
-        need to be escaped.
-
-        Run `unit list` to see the available symbols.
-
         EXAMPLES:
-
-        unit convert 1_ft m
-        unit convert 5.4_kW\\*hr J
-        unit convert 5.4e-3_km/s mi/hr
-        unit convert "12 kg*m/s^2" "N"
-        unit convert 12_m^1\\*s^-1\\*kg^1 kg\\*m/s
+          unit convert 1_ft m
+          unit convert 1_ft meter
+          unit convert 5.4_kW\\*hr J
+          unit convert 5.4e-3_km/s mi/hr
+          unit convert "12 kg*m/s^2" "N"
+          unit convert 12_m^1\\*s^-1\\*kg^1 kg\\*m/s
         """
     )
 
-    @Argument(help: "The measurement to convert from")
+    @Argument(help: """
+    The measurement to convert. This is a number, followed by a space, followed by a unit \
+    symbol. For convenience, you may use an underscore `_` to represent the space.
+    """)
     var from: Measurement
 
-    @Argument(help: "The unit to convert from")
+    @Argument(help: """
+    The unit to convert to. This can either be a unit name, a unit symbol, or an equation of \
+    unit symbols.
+    """)
     var to: Units.Unit
 
     func run() throws {
